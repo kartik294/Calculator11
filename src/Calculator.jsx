252cf01913bc25ec53ba1cc018bc72857a15d85c
kartik294 <kartikgeu@gmail.com>
2024-06-18 16:31:19 +0530

    third commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -3,27 +3,29 @@ import './Calculator.css'; // Import the CSS file
 
 const Calculator = () => {
   const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
 
   const handleButtonClick = (value) => {
     if (value === 'C') {
       setInput('');
+      setResult('');
     } else if (value === '=') {
       try {
         if (input.trim() === '') {
-          setInput('Error');
+          setResult('Error');
         } else {
           // eslint-disable-next-line no-eval
-          const result = eval(input);
-          if (result === Infinity) {
-            setInput('Infinity');
-          } else if (isNaN(result)) {
-            setInput('NaN');
+          const calculatedResult = eval(input);
+          if (calculatedResult === Infinity) {
+            setResult('Infinity');
+          } else if (isNaN(calculatedResult)) {
+            setResult('NaN');
           } else {
-            setInput(result.toString());
+            setResult(calculatedResult.toString());
           }
         }
       } catch {
-        setInput('Error');
+        setResult('Error');
       }
     } else {
       setInput(input + value);
@@ -32,29 +34,28 @@ const Calculator = () => {
 
   return (
     <>
-     <h1 className="heading">React Calculator</h1>
-    <div className="calculator">
-    
-      <input className="input" type="text" value={input} readOnly />
-      <button className="button" onClick={() => handleButtonClick('7')}>7</button>
-      <button className="button" onClick={() => handleButtonClick('8')}>8</button>
-      <button className="button" onClick={() => handleButtonClick('9')}>9</button>
-      <button className="button operator" onClick={() => handleButtonClick('+')}>+</button>
-      <button className="button" onClick={() => handleButtonClick('4')}>4</button>
-      <button className="button" onClick={() => handleButtonClick('5')}>5</button>
-      <button className="button" onClick={() => handleButtonClick('6')}>6</button>
-      <button className="button operator" onClick={() => handleButtonClick('-')}>-</button>
-      <button className="button" onClick={() => handleButtonClick('1')}>1</button>
-      <button className="button" onClick={() => handleButtonClick('2')}>2</button>
-      <button className="button" onClick={() => handleButtonClick('3')}>3</button>
-      <button className="button operator" onClick={() => handleButtonClick('*')}>*</button>
-      <button className="button" onClick={() => handleButtonClick('C')}>C</button>
-      <button className="button" onClick={() => handleButtonClick('0')}>0</button>
-      <button className="button" onClick={() => handleButtonClick('=')}>=</button>
-      <button className="button operator" onClick={() => handleButtonClick('/')}>/</button>
-    </div>
+      <h1 className="heading">React Calculator</h1>
+      <div className="calculator">
+        <input className="input" type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+        <button className="button" onClick={() => handleButtonClick('7')}>7</button>
+        <button className="button" onClick={() => handleButtonClick('8')}>8</button>
+        <button className="button" onClick={() => handleButtonClick('9')}>9</button>
+        <button className="button operator" onClick={() => handleButtonClick('+')}>+</button>
+        <button className="button" onClick={() => handleButtonClick('4')}>4</button>
+        <button className="button" onClick={() => handleButtonClick('5')}>5</button>
+        <button className="button" onClick={() => handleButtonClick('6')}>6</button>
+        <button className="button operator" onClick={() => handleButtonClick('-')}>-</button>
+        <button className="button" onClick={() => handleButtonClick('1')}>1</button>
+        <button className="button" onClick={() => handleButtonClick('2')}>2</button>
+        <button className="button" onClick={() => handleButtonClick('3')}>3</button>
+        <button className="button operator" onClick={() => handleButtonClick('*')}>*</button>
+        <button className="button" onClick={() => handleButtonClick('C')}>C</button>
+        <button className="button" onClick={() => handleButtonClick('0')}>0</button>
+        <button className="button" onClick={() => handleButtonClick('=')}>=</button>
+        <button className="button operator" onClick={() => handleButtonClick('/')}>/</button>
+      </div>
     </>
-   
   );
 };
 
